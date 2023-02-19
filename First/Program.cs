316e1654c4524d0a7fbf85349aaa617b124b4d7a
namedmichi <j.technology@web.de>
2023-02-19 12:43:20 +0100

    sync

--- First/Program.cs
+++ First/Program.cs
@@ -10,24 +10,33 @@ namespace HelloWorld
             Console.WriteLine("Hello World!");
             object[,] obj =
             {
-                {"Michi", 17 + " jr"},
-                {"Michael", 30 + " sn"}
+                {"Michi", 17 , " jr"},
+                {"Michael", 30 , " sn"}
             };
 
-            for (int i = 0; i < 2; i++)
-            {
+            //for (int i = 0; i < 2; i++)
+            //{
 
-                for (int j = 0; j < 2; j++)
-                {
-                    
-                    Console.Write(obj[i, j] + " ");
-                    
-                    
+            //    for (int j = 0; j < 2; j++)
+            //    {
 
-                }
-                Console.WriteLine();
-            }
+            //        Console.Write(obj[i, j] + " ");
 
+
+
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            printRow(0, obj);
+            printRow(1, obj);
+
+        }
+
+        static void printRow(int rowNumber, object[,] o)
+        {
+            Console.Write(o[rowNumber, 0] + " " + o[rowNumber, 1] + " " + o[rowNumber, 2] + " ");
+            Console.WriteLine();
         }
     }
 }
