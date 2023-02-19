57b79aea42fdfffa49b51c055b1364c9e415b2a3
namedmichi <j.technology@web.de>
2023-02-19 12:25:33 +0100

    Projektdateien hinzufügen.

---
+++ First/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.Common;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            object[,] obj =
+            {
+                {"Michi", 17 + "jr"},
+                {"Michael", 30 + "sn"}
+            };
+
+            for (int i = 0; i < 1; i++)
+            {
+
+                for (int j = 0; j < 2; j++)
+                {
+                    
+                    Console.Write(obj[i, j] + " ");
+                    
+                    
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
