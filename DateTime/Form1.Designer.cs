4f706400633400ff8bf32932a349f844d9e19bd7
namedmichi <j.technology@web.de>
2023-03-06 15:54:20 +0100

    sync

--- DateTime/Form1.Designer.cs
+++ DateTime/Form1.Designer.cs
@@ -30,16 +30,23 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxNr = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(331, 119);
+            this.button1.Location = new System.Drawing.Point(498, 196);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(138, 48);
             this.button1.TabIndex = 0;
             this.button1.Text = "Zeit Stoppen";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
@@ -47,14 +54,76 @@
             this.textBox1.Location = new System.Drawing.Point(104, 160);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(140, 200);
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(177, 200);
             this.textBox1.TabIndex = 2;
+            this.textBox1.Visible = false;
+            // 
+            // textBoxNr
+            // 
+            this.textBoxNr.Location = new System.Drawing.Point(343, 211);
+            this.textBoxNr.Name = "textBoxNr";
+            this.textBoxNr.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNr.TabIndex = 3;
+            this.textBoxNr.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(346, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nr.";
+            this.label1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(266, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(263, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Teilnehmer Anzahl";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(400, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Bestätigen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Bestaetigen_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(110, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "0/x Teilnehmern sind schon im Ziel";
+            this.label3.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxNr);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -68,6 +137,12 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxNr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- DateTime/Form1.cs
+++ DateTime/Form1.cs
@@ -13,11 +13,11 @@ namespace DateTime
     public partial class Form1 : Form
     {
 
-
+        int teil = 0;
         int c = 0;
         string newLine = Environment.NewLine;
 
-        String[] zeit = new String[13];
+        List<string> zeit = new List<string>();
         public Form1()
         {
             InitializeComponent();
@@ -25,15 +25,26 @@ namespace DateTime
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (c == 13 ) 
+            if (c == teil ) 
             {
                 MessageBox.Show("Liste voll");
                 return;
              }
-            zeit[c]= System.DateTime.Now.ToString();
-            textBox1.Text = textBox1.Text + newLine + zeit[c].ToString();
-            
+            zeit.Add(System.DateTime.Now.TimeOfDay.ToString());
+            label3.Text = c+1 + "/" + teil + " Teilnehmern sind schon im Ziel";
+            textBox1.Text = textBox1.Text + newLine + "Nr." + textBoxNr.Text.ToString() + "       " + zeit[c].ToString();
+            label3.Visible= true;
             c++;
+        }
+
+        private void Bestaetigen_Click(object sender, EventArgs e)
+        {
+            if  (textBox2.Text != "" )
+            {
+                teil = int.Parse(textBox2.Text);
+                textBox1.Visible= true; textBoxNr.Visible= true; label1.Visible= true;button1.Visible= true; 
+                textBox2.Visible= false;label2.Visible= false;button2.Visible= false;
+            }
         }
     }
 }
