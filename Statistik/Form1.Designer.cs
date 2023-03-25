69f3d12271b48f4410ef428143635a0dff498a1d
namedmichi <j.technology@web.de>
2023-03-25 18:54:49 +0100

    push

--- Statistik/Form1.Designer.cs
+++ Statistik/Form1.Designer.cs
@@ -33,6 +33,8 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.zurück = new System.Windows.Forms.Button();
+            this.set = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -43,15 +45,15 @@
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Location = new System.Drawing.Point(-8, 25);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(284, 363);
+            this.pictureBox1.Size = new System.Drawing.Size(284, 511);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(184, 34);
+            this.button1.Location = new System.Drawing.Point(177, 30);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(76, 354);
+            this.button1.Size = new System.Drawing.Size(76, 500);
             this.button1.TabIndex = 1;
             this.button1.UseVisualStyleBackColor = true;
             // 
@@ -73,27 +75,50 @@
             this.button3.TabIndex = 3;
             this.button3.Text = "-";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(400, 60);
+            this.textBox1.Location = new System.Drawing.Point(461, 60);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 4;
             // 
+            // zurück
+            // 
+            this.zurück.Location = new System.Drawing.Point(656, 86);
+            this.zurück.Name = "zurück";
+            this.zurück.Size = new System.Drawing.Size(188, 60);
+            this.zurück.TabIndex = 5;
+            this.zurück.Text = "Zurück";
+            this.zurück.UseVisualStyleBackColor = true;
+            this.zurück.Click += new System.EventHandler(this.zurück_Click);
+            // 
+            // set
+            // 
+            this.set.Location = new System.Drawing.Point(656, 330);
+            this.set.Name = "set";
+            this.set.Size = new System.Drawing.Size(132, 68);
+            this.set.TabIndex = 6;
+            this.set.Text = "Set";
+            this.set.UseVisualStyleBackColor = true;
+            this.set.Click += new System.EventHandler(this.set_Click);
+            // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(550, 60);
+            this.textBox2.Location = new System.Drawing.Point(535, 355);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 5;
+            this.textBox2.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(868, 575);
             this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.set);
+            this.Controls.Add(this.zurück);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -114,6 +139,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button zurück;
+        private System.Windows.Forms.Button set;
         private System.Windows.Forms.TextBox textBox2;
     }
 }
--- Statistik/Form1.cs
+++ Statistik/Form1.cs
@@ -12,14 +12,89 @@ namespace Statistik
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+            
         }
+        int fixPos = 527;
+        List<int> reverse = new List<int>();
+        int heigt = 500;
 
         private void button2_Click(object sender, EventArgs e)
         {
-            button1.Height = 2 ;
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Zahl eingeben");
+                return;
+            }
+            if(heigt + (5 * int.Parse(textBox1.Text)) > 500)
+            {
+                MessageBox.Show("Maximale Höhe erreicht");
+                return;
+            }
+            button1.Location = new Point(button1.Location.X, button1.Location.Y - (5*int.Parse(textBox1.Text)));
+            button1.Height = button1.Height + (5 * int.Parse(textBox1.Text));
+            reverse.Add(-(5 * int.Parse(textBox1.Text)));
+            heigt += (5 * int.Parse(textBox1.Text));
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if(textBox1.Text == "")
+            {
+                MessageBox.Show("Zahl eingeben");
+                return; 
+            }
+            if (heigt - (5 * int.Parse(textBox1.Text)) <= 0)
+            {
+                MessageBox.Show("Minimale Höhe erreicht");
+                return;
+            }
+            button1.Location = new Point(button1.Location.X, button1.Location.Y + (5 *int.Parse(textBox1.Text)));
+            button1.Height = button1.Height - (5 * int.Parse(textBox1.Text));
+            reverse.Add((5 * int.Parse(textBox1.Text)));
+            heigt -= (5 * int.Parse(textBox1.Text));
+        }
+
+        private void zurück_Click(object sender, EventArgs e)
+        {
+            if(reverse.Count== 0)
+            {
+                MessageBox.Show("Es gibt keine vorherigen werte mehr");
+                return;
+            }
+           int zurück = reverse[reverse.Count - 1];
+           button1.Location = new Point(button1.Location.X, button1.Location.Y - zurück);
+           button1.Height = button1.Height + zurück;
+            heigt = button1.Height;
+            reverse.RemoveAt(reverse.Count - 1);
+
+        }
+
+        private void set_Click(object sender, EventArgs e)
+        {
+            if((5 * int.Parse(textBox2.Text)) > 500)
+            {
+                MessageBox.Show("Zu groß");
+                return; 
+            }
+            if(button1.Height > (5 * int.Parse(textBox2.Text)))
+            {
+                reverse.Add((button1.Height - (5 * int.Parse(textBox2.Text))));
+            }
+            else
+            {
+                reverse.Add( -(5 * int.Parse(textBox2.Text)- button1.Height));
+            }
+            button1.Height = (5 * int.Parse(textBox2.Text));
+              
+            int offset = button1.Height;
+            heigt = button1.Height;
+            button1.Location = new Point(button1.Location.X, fixPos - offset);
+        }
+
     }
 }
