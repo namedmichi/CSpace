6ea826a3c79e920427813587ca368b8116a20deb
namedmichi <j.technologyeb.de>
2023-04-19 09:19:03 +0200

    push

--- Zeichnen/Form1.Designer.cs
+++ Zeichnen/Form1.Designer.cs
@@ -28,13 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "red",
+            "green",
+            "black",
+            "blue"});
+            this.listBox1.Location = new System.Drawing.Point(106, 260);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseClick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -45,6 +61,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Zeichnen/Form1.cs
+++ Zeichnen/Form1.cs
@@ -15,6 +15,8 @@ namespace Zeichnen
         Graphics g;
         Point start = new Point(0, 0);
         Point end;
+        Pen pen = Pens.Black;
+        bool first = true;
         public Form1()
         {
             InitializeComponent();
@@ -27,14 +29,52 @@ namespace Zeichnen
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
+            if (first)
+            {
+                end = new Point(e.X, e.Y);
+                start = end;
+                first = false;
+            }
+            if(e.Button == MouseButtons.Left)
+            {
             start = end;
             end = new Point(e.X, e.Y);
-            g.DrawLine(Pens.Black, start, end);
+            g.DrawLine(pen, start, end);
+
+            }
+            if(e.Button == MouseButtons.Right)
+            {
+                first  = true;
+            }
 
         }
 
         private void Form1_MouseUp(object sender, MouseEventArgs e)
         {
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+           if( listBox1.SelectedIndex == 0 )
+            {
+                MessageBox.Show(listBox1.SelectedIndex.ToString());
+                pen = Pens.Red;
+            } else if ( listBox1.SelectedIndex == 1 )
+            {
+                pen = Pens.Green;
+            }else if ( listBox1.SelectedIndex == 2 )
+            {
+                pen = Pens.Black;
+            }
+            else
+            {
+                pen = Pens.Blue;
+            }
+
         }
     }
 }
