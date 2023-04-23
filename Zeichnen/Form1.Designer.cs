c5f6c759512adc86d9d73efb686800f0ff86e134
namedmichi <j.technology@web.de>
2023-04-23 13:15:38 +0200

    push

--- Zeichnen/Form1.Designer.cs
+++ Zeichnen/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // listBox1
@@ -39,30 +42,69 @@
             "green",
             "black",
             "blue"});
-            this.listBox1.Location = new System.Drawing.Point(106, 260);
+            this.listBox1.Location = new System.Drawing.Point(24, 343);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(120, 95);
             this.listBox1.TabIndex = 0;
             this.listBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseClick);
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 308);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 287);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Stiftdicke";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "Linie",
+            "4 Eck",
+            "3 Eck"});
+            this.listBox2.Location = new System.Drawing.Point(24, 189);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 3;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(906, 515);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.ClientSizeChanged += new System.EventHandler(this.Form1_ClientSizeChanged);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
             this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox2;
     }
 }
 
--- Zeichnen/Form1.cs
+++ Zeichnen/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,7 +16,7 @@ namespace Zeichnen
         Graphics g;
         Point start = new Point(0, 0);
         Point end;
-        Pen pen = Pens.Black;
+        Pen pen = new Pen(Brushes.Black);
         bool first = true;
         public Form1()
         {
@@ -27,25 +28,109 @@ namespace Zeichnen
             g = this.CreateGraphics();
         }
 
+        int x;
+        int y;
+        int width;
+        int height;
+        int clicks;
+        Point Point1;
+        Point Point2;
+        Point Point3;
+
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
-            if (first)
+            if ( listBox2.SelectedIndex == 0)
             {
-                end = new Point(e.X, e.Y);
+                if (first)
+                {
+                    end = new Point(e.X, e.Y);
+                    start = end;
+                    first = false;
+                }
+                if(e.Button == MouseButtons.Left)
+                {
                 start = end;
-                first = false;
-            }
-            if(e.Button == MouseButtons.Left)
+                end = new Point(e.X, e.Y);
+                g.DrawLine(pen, start, end);
+
+                }
+                if(e.Button == MouseButtons.Right)
+                {
+                    first  = true;
+                }
+            }else if ( listBox2.SelectedIndex == 1)
             {
-            start = end;
-            end = new Point(e.X, e.Y);
-            g.DrawLine(pen, start, end);
+                if (first)
+                {
+                    x = e.X;
+                    y = e.Y;
+                    clicks++;
+                    first = false;
+                    return;
+
+                }
+                if  (clicks == 1)
+                {
+                    width = e.X - x;
+                    if (width < 0)
+                    {
+                        width = width * -1;
+                        x -= width;
+                    }
+                    clicks++;
+                    return;
+                }
+                if(clicks == 2)
+                {
+                    height = e.Y - y;
+                    if (height < 0)
+                    {
+                        height = height * -1;
+                        y -= height;
+                    }
+                    g.DrawRectangle(pen, x, y, width, height); 
+                    first = true;
+                    clicks = 0;
+                }
+            }else if ( listBox2.SelectedIndex == 2)
+            {
+                if ( clicks == 0)
+                {
+
+                    Point1 = new Point(e.X, e.Y);
+                    clicks++;
+                    return;
+                }
+                if (clicks == 1)
+                {
+
+                    Point2 = new Point(e.X, e.Y);
+                    clicks++;
+                    return;
+                }
+                if (clicks == 2)
+                {
+
+                    Point3 = new Point(e.X, e.Y);       
+                    Point[] curvePoints =
+                                     {
+                                 Point1,
+                                 Point2,
+                                 Point3
+            
+              
+                    };
+
+                    // Draw polygon to screen.
+                    g.DrawPolygon(pen, curvePoints);
+                    clicks = 0;
+                }
+
+
+        
 
             }
-            if(e.Button == MouseButtons.Right)
-            {
-                first  = true;
-            }
+
 
         }
 
@@ -62,19 +147,54 @@ namespace Zeichnen
            if( listBox1.SelectedIndex == 0 )
             {
                 MessageBox.Show(listBox1.SelectedIndex.ToString());
-                pen = Pens.Red;
+                pen = new Pen(Brushes.Red);
+                pen.Width = 5;
             } else if ( listBox1.SelectedIndex == 1 )
             {
-                pen = Pens.Green;
+                pen = new Pen(Brushes.Green);
             }else if ( listBox1.SelectedIndex == 2 )
             {
-                pen = Pens.Black;
+                pen = new Pen(Brushes.Black);
             }
             else
             {
-                pen = Pens.Blue;
+                pen = new Pen(Brushes.Blue);
             }
 
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Length <= 0)
+            {
+                return;
+            }
+            try
+            {
+            pen.Width = int.Parse(textBox1.Text);
+
+            }
+            catch 
+            {
+                MessageBox.Show("Nur Zahlen!!!");
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            first = true;
+        }
+
+        private void Form1_ClientSizeChanged(object sender, EventArgs e)
+        {
+            
+            Thread.Sleep(2000); 
+            Form1.ActiveForm.ClientSize = new Size(this.width, this.height);
         }
     }
 }
