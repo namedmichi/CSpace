993bbba5efd899434882407483ca8469965e55eb
namedmichi <j.technologyeb.de>
2023-05-08 11:08:21 +0200

    push

--- Zeichnen 2/Form1.Designer.cs
+++ Zeichnen 2/Form1.Designer.cs
@@ -28,7 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.speichernToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.öffnenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(762, 451);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.speichernToolStripMenuItem,
+            this.öffnenToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // speichernToolStripMenuItem
+            // 
+            this.speichernToolStripMenuItem.Name = "speichernToolStripMenuItem";
+            this.speichernToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.speichernToolStripMenuItem.Text = "Speichern";
+            this.speichernToolStripMenuItem.Click += new System.EventHandler(this.speichernToolStripMenuItem_Click);
+            // 
+            // öffnenToolStripMenuItem
+            // 
+            this.öffnenToolStripMenuItem.Name = "öffnenToolStripMenuItem";
+            this.öffnenToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.öffnenToolStripMenuItem.Text = "öffnen";
+            this.öffnenToolStripMenuItem.Click += new System.EventHandler(this.öffnenToolStripMenuItem_Click);
             // 
             // Form1
             // 
@@ -36,17 +78,25 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
-            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem speichernToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem öffnenToolStripMenuItem;
     }
 }
 
--- Zeichnen 2/Form1.cs
+++ Zeichnen 2/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace Zeichnen_2
 {
     public partial class Form1 : Form
     {
+        Bitmap bitmap;
         Graphics g;
         bool mouseRIsDown;
         bool mouseLIsDown;
@@ -24,17 +26,55 @@ namespace Zeichnen_2
         }
 
         private void Form1_Load(object sender, EventArgs e)
-        {
-            g = this.CreateGraphics();
+        {   
+            //g = this.CreateGraphics();
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bitmap);
+            g.FillRectangle(Brushes.White, 0, 0, pictureBox1.Width, pictureBox1.Height);
         }
 
-        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        private void save_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseRIsDown = true;
+            }
+            else
+            {
+                mouseLIsDown = true;
+            }
+           
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseRIsDown = false;
+                lastx = 0;
+                lasty = 0;
+            }
+            else
+            {
+                mouseLIsDown = false;
+                mouseLIsDown = false;
+                lastx = 0;
+                lasty = 0;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
             if (mouseRIsDown)
             {
+                pictureBox1.Image = bitmap;
                 Point point = this.PointToClient(Cursor.Position);
                 g.FillRectangle(Brushes.Black, point.X, point.Y, 2, 2);
-                if(lastx == 0 && lasty == 0)
+                if (lastx == 0 && lasty == 0)
                 {
                     lastx = point.X;
                     lasty = point.Y;
@@ -45,8 +85,9 @@ namespace Zeichnen_2
                 lasty = point.Y;
 
             }
-            else if ( mouseLIsDown)
+            else if (mouseLIsDown)
             {
+                pictureBox1.Image = bitmap;
                 Point point = this.PointToClient(Cursor.Position);
                 g.FillRectangle(Brushes.White, point.X, point.Y, 6, 6);
                 if (lastx == 0 && lasty == 0)
@@ -60,34 +101,30 @@ namespace Zeichnen_2
                 lastx = point.X;
                 lasty = point.Y;
             }
+
         }
 
-        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        private void speichernToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if( e.Button == MouseButtons.Left)
+            try
             {
-             mouseRIsDown = true;
+
+            bitmap.Save("C:\\Users\\selbertinger\\Desktop\\workspaces\\CSpace\\Zeichnen\\test.jpg", ImageFormat.Jpeg);
+                
             }
-            else
+            catch
             {
-                mouseLIsDown = true;
+                bitmap.Dispose();
+                bitmap.Save("C:\\Users\\selbertinger\\Desktop\\workspaces\\CSpace\\Zeichnen\\test.jpg", ImageFormat.Jpeg);
+         
             }
         }
 
-        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        private void öffnenToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (e.Button == MouseButtons.Left)
-            {
-                mouseRIsDown = false;
-                lastx = 0;
-                lasty = 0;
-            }
-            else
-            {
-                mouseLIsDown = false;
-                lastx = 0;
-                lasty = 0;
-            }
+            bitmap = new Bitmap("C:\\Users\\selbertinger\\Desktop\\workspaces\\CSpace\\Zeichnen\\test.jpg");
+            pictureBox1.Image = bitmap;
+            g = Graphics.FromImage(bitmap);
         }
     }
 }
